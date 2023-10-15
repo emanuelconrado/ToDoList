c358fc9885d9a1d4bab61fb340073d0e5cb73ff5
xxDuel <emanuelthadeu45@gmail.com>
2023-10-15 14:30:03 -0300

    add bottom

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -15,13 +15,34 @@ class ViewController: UIViewController, UITableViewDataSource {
         return table
     }()
     
+    var items = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "To Do List"
         view.addSubview(table)
         table.dataSource = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
     }
 
+    @objc private func didTapAdd(){
+        let alert = UIAlertController(title: "New Item", message: "Enter new to do list item", preferredStyle: .alert)
+        alert.addTextField {field in field.placeholder = "Enter item..." }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: {[weak self]
+            (_) in if let field = alert.textFields?.first{
+                if let text = field.text, !text.isEmpty {
+                    // Enter new to do list item
+                    DispatchQueue.main.async {
+                        self?.items.append(text)
+                        self?.table.reloadData()
+                    }
+                }
+            }
+        }))
+        present(alert, animated: true)
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         table.frame = view.bounds
@@ -29,11 +50,12 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return items.count
     }
 }
 
